3b6456610b1461c317ad0e972e1cb4d78ce56454
bu11ad <145846993+bu11ad@users.noreply.github.com>
2023-09-24 12:10:25 +0400

    script.js

--- script.js
+++ script.js
@@ -1,9 +1,46 @@
+var massiveText = [
+	'Важно помнить одно правило Кекс',
+	'Ты заблудилься? Нет ты нашел путь к Кексам',
+	'Кликай Кликай я все равно ничего не получу',
+	'Кекс Вика выбери меня',
+	'Шоколаде мармеладе это шедевральный Кексиаде',
+	'Я супер пупер секси я шоколадный Кексик',
+	'Все ребят обновлений не будет у создателя передоз Кексом'
+];
+var randomNumber = Math.floor(Math.random() * 7);
+alert(massiveText[randomNumber]);
 
 
+var shop = document.getElementById('openShop');
+var back = document.getElementById('back');
 var h1Id = document.getElementById('h1');
 var buttonId = document.getElementById('button');
+var upgrage1button = document.getElementById('upgrade1');
+var much1 = 10;
 var counter = 0;
-buttonId.addEventListener('click', function () {
-	counter++;
-	h1Id.textContent = 'Кексы:' + counter;
+var upgrade0 = 1;
+shop.addEventListener('click', function () {
+	shop.classList.add('hidden');
+	back.classList.remove('hidden');
+	upgrage1button.classList.remove('hidden');
 });
+
+back.addEventListener('click', function () {
+	back.classList.add('hidden');
+	upgrage1button.classList.add('hidden');
+	shop.classList.remove('hidden');
+});
+
+upgrage1button.addEventListener('click', function () {
+	counter = counter - much1;
+	upgrade0 = upgrade0 + 1;
+	h1Id.textContent = 'Cakes:' + counter;
+	much1 = much1 + 10;
+	upgrage1button.textContent = 'buy +1 click (' + much1 + ')'
+});
+
+buttonId.addEventListener('click', function () {
+	counter = counter + upgrade0;
+	h1Id.textContent = 'Cakes:' + counter;
+});
+
