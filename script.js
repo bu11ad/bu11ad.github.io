169ed531e1d59e112b2eb60885f1555e4bfd008d
bu11ad <145846993+bu11ad@users.noreply.github.com>
2023-09-25 23:18:51 +0400

    Add files via upload

--- script.js
+++ script.js
@@ -5,7 +5,8 @@ var massiveText = [
 	'Кекс Вика выбери меня',
 	'Шоколаде мармеладе это шедевральный Кексиаде',
 	'Я супер пупер секси я шоколадный Кексик',
-	'Все ребят обновлений не будет у создателя передоз Кексом'
+	'Все ребят обновлений не будет у создателя передоз Кексом',
+	'Нашли баг? Сообщите Кексу вот мой дс bubad'
 ];
 var randomNumber = Math.floor(Math.random() * 7);
 alert(massiveText[randomNumber]);
@@ -17,13 +18,27 @@ var backSettings = document.getElementById('backSettings');
 var h1Id = document.getElementById('h1');
 var buttonId = document.getElementById('button');
 var upgrade1button = document.getElementById('upgrade1');
+let upgradeButtonPerSecond1 = document.getElementById('upgrade2');
 var grey = document.getElementById('grey');
 var popUp = document.getElementById('popUp');
 var popUpClose = document.getElementById('close');
 var popUpText = document.getElementById('mistake');
 var much1 = parseInt(localStorage.getItem('much'));
+var much2 = parseInt(localStorage.getItem('much2'));
 var counter = parseInt(localStorage.getItem('count'));
-var upgrade0 = parseInt(localStorage.getItem('upgrade'));
+var upgrade0 = parseInt(localStorage.getItem('upgradeClick'));
+var upgrade1 = parseInt(localStorage.getItem('upgradeSec'));
+var form = document.getElementById('form');
+var picker = document.getElementById('colorPickerId');
+var submitFormPicker = document.getElementById('colorPickerSubmit');
+let massiveWhite=document.getElementsByClassName('white');
+let clickPerSecond = document.getElementById('clickPerSecond');
+submitFormPicker.addEventListener('click',function(event){
+	event.preventDefault();
+	for(var z=0;z<massiveWhite.length;z++){
+		massiveWhite[z].style.backgroundColor=picker.value;
+	}
+});
 if(localStorage.getItem('count')){
 	var counter = parseInt(localStorage.getItem('count'));
 	h1Id.textContent = 'Cakes:' + counter;
@@ -32,13 +47,22 @@ if(localStorage.getItem('count')){
 else{
 	var counter = 0;
 }
-if(localStorage.getItem('upgrade')){
-	var upgrade0 = parseInt(localStorage.getItem('upgrade'));
+if(localStorage.getItem('upgradeClick')){
+	var upgrade0 = parseInt(localStorage.getItem('upgradeClick'));
 	h1Id.textContent = 'Cakes:' + counter;
 	upgrade1button.textContent = 'buy +1 click (' + much1 + ')'
 }
 else{
 	var upgrade0 = 1;
+}
+if(localStorage.getItem('upgradeSec')){
+	var upgrade1 = parseInt(localStorage.getItem('upgradeSec'));
+	h1Id.textContent = 'Cakes:' + counter;
+	clickPerSecond.textContent = 'Clicks in Second: ' + upgrade1;
+	upgradeButtonPerSecond1.textContent = 'buy click in Sec(' + much2 + ')';
+}
+else{
+	var upgrade1 = 1;
 }
 if(localStorage.getItem('much')){
 	var much1 = parseInt(localStorage.getItem('much'));
@@ -47,7 +71,25 @@ if(localStorage.getItem('much')){
 else{
 	var much1 = 10;
 }
+if(localStorage.getItem('much2')){
+	var much2 = parseInt(localStorage.getItem('much2'));
+	h1Id.textContent = 'Cakes:' + counter;
+}
+else{
+	var much2 = 100;
+}
 
+
+function clickPerTime(){
+	counter = counter + upgrade1;
+	h1Id.textContent = 'Cakes:' + counter;
+	localStorage.setItem('count', counter);
+	localStorage.setItem('upgradeClick', upgrade0);
+	localStorage.setItem('upgradeSec', upgrade1);
+	localStorage.setItem('much', much1);
+	localStorage.setItem('much2', much2);
+}
+setInterval(clickPerTime, 1000);
 popUpClose.addEventListener('click', function () {
 	popUp.classList.add('hidden');
 	grey.classList.add('hidden');
@@ -67,12 +109,14 @@ shop.addEventListener('click', function () {
 	settings.classList.add('hidden');
 	back.classList.remove('hidden');
 	upgrade1button.classList.remove('hidden');
+	upgradeButtonPerSecond1.classList.remove('hidden');
 	
 });
 
 back.addEventListener('click', function () {
 	back.classList.add('hidden');
 	upgrade1button.classList.add('hidden');
+	upgradeButtonPerSecond1.classList.add('hidden');
 	shop.classList.remove('hidden');
 	settings.classList.remove('hidden');
 });
@@ -81,12 +125,29 @@ settings.addEventListener('click', function () {
 	shop.classList.add('hidden');
 	settings.classList.add('hidden');
 	backSettings.classList.remove('hidden');
+	form.classList.remove('hidden');
 });
 
 backSettings.addEventListener('click', function () {
 	backSettings.classList.add('hidden');
 	settings.classList.remove('hidden');
 	shop.classList.remove('hidden');
+	form.classList.add('hidden');
+});
+
+upgradeButtonPerSecond1.addEventListener('click', function () {
+	if(much2>counter){
+		popUp.classList.remove('hidden');
+		grey.classList.remove('hidden');
+		popUpText.textContent = ' Error! do you think you can buy an upgrade when you dont have the money for it? You re wrong son!';
+	}
+	else{
+	counter = counter - much2;
+	upgrade1 = upgrade1 + 1;
+	h1Id.textContent = 'Cakes:' + counter;
+	much2 = much2 + 100;
+	upgradeButtonPerSecond1.textContent = 'buy click in Sec(' + much2 + ')'
+	}
 });
 
 upgrade1button.addEventListener('click', function () {
@@ -108,7 +169,14 @@ buttonId.addEventListener('click', function () {
 	counter = counter + upgrade0;
 	h1Id.textContent = 'Cakes:' + counter;
 	localStorage.setItem('count', counter);
-	localStorage.setItem('upgrade', upgrade0);
+	localStorage.setItem('upgradeClick', upgrade0);
+	localStorage.setItem('upgradeSec', upgrade1);
 	localStorage.setItem('much', much1);
+	localStorage.setItem('much2', much2);
 });
+
+upgradeButtonPerSecond1.addEventListener('click', function(){
+	clickPerSecond.textContent = "Clicks in Second: " + upgrade1;
+});
+
 
